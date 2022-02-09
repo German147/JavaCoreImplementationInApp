7a342ee5b224b753e3d894c0c60d4be080304888
Germán <barreragerman27@gmail.com>
2022-02-08 23:19:56 -0300

    Exceptions practice - Here it was implemented RuntimeExceptions

---
+++ src/main/java/com/germanbarrera/controlClientes/entity/People.java
@@ -0,0 +1,60 @@
+package com.germanbarrera.controlClientes.entity;
+
+public class People {
+    String name;
+    String surname;
+    String email;
+    String phone;
+
+    public People() {
+    }
+
+    public People(String name, String surname, String email, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
